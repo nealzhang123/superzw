1ff176bc9b57f69547f9323d875c9e2e29b1ba21
nealzhang123 <nealzhangwei@gmail.com>
2016-10-10 20:12:42 +0800

    update template

---
+++ wp-content/themes/thbusiness/layouts/company-contact.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: 联系我们模板
+ *
+ * Displays the Test Template of the theme.
+ *
+ * @package thbusiness
+ */
+get_header(); ?>
+<div class="col-xs-12 col-sm-12 col-md-12">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			联系我们模板,整理中...
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- .col-xs-12 col-sm-12 col-md-12 -->
+<?php get_footer(); ?>
---
+++ wp-content/themes/thbusiness/layouts/company-pro.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: 公司产品模板
+ *
+ * Displays the Test Template of the theme.
+ *
+ * @package thbusiness
+ */
+get_header(); ?>
+<div class="col-xs-12 col-sm-12 col-md-12">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			公司产品模板,整理中...
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- .col-xs-12 col-sm-12 col-md-12 -->
+<?php get_footer(); ?>
