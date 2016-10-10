29ced3363295095df143fc75a9882f11136d5bb8
nealzhang123 <nealzhangwei@gmail.com>
2016-10-10 20:04:42 +0800

    heihai theme

---
+++ wp-content/themes/thbusiness/layouts/main-page.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: 首页模板
+ *
+ * Displays the Test Template of the theme.
+ *
+ * @package thbusiness
+ */
+get_header(); ?>
+<div class="col-xs-12 col-sm-12 col-md-12">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="homenews">
+		      <div class="titles">
+		        <h2 class="fl">
+		        	<a href="<?php echo site_url('公司要闻'); ?>">要闻快报</a>
+		        	<div class="fr more"> <a class="a-more iconbg" href="<?php echo site_url('公司要闻'); ?>" title="更多"><img src="http://www.crc.com.cn/images/i05.gif" /></a>
+		        	</div>
+		        </h2>
+		        
+		      </div>
+		      <div class="homemain-conts homenews-conts" style="border:none;">
+		       
+		        <div class="row">
+		          <div class="col-xs-3 col-sm-3 col-md-3">
+		          	<img src="http://www.crc.com.cn/images/crc_default.gif" />
+		          </div>
+		          <div class="col-xs-9 col-sm-9 col-md-9">
+		            <h3 style="margin: 0px;"><a href="http://winfo.crc.com.cn/news/crc_dynamic/201610/t20161004_401423.htm" target="_blank">第五届监事会进驻华润集团</a></h3>
+		            <p> 9月28日上午，华润集团召开国务院派驻第五届监事会进驻会议。李兵主席率监事会正式进驻华润集团并作重要讲话，集团傅育宁董事长出席会议并做表态发言，罗熹总经理向监事会介绍了集团经营管理工作情况。监事会第26...<a class="org" href="http://winfo.crc.com.cn/news/crc_dynamic/201610/t20161004_401423.htm" target="_blank"><img src="http://www.crc.com.cn/images/i08.gif" alt="详细"></a></p>
+		          </div>
+		        </div>
+		      
+		        <div class="homenews-list">
+		            <ul class="two_col_list">
+				   		<li><a href="http://winfo.crc.com.cn/news/crc_dynamic/201610/t20161003_401387.htm" target="_blank">罗熹总经理会见邮政集团李国华总经理</a></li>
+		                <li><a href="http://winfo.crc.com.cn/news/crc_dynamic/201609/t20160930_401371.htm" target="_blank">集团召开2016年度财务经理人会议</a></li>
+		                <li><a href="http://winfo.crc.com.cn/news/crc_dynamic/201609/t20160928_401076.htm" target="_blank">傅育宁董事长会见湖南省张剑飞副省长</a></li> 
+		            </ul>
+		     		<ul class="two_col_list">
+		               <li><a href="http://winfo.crc.com.cn/news/profit/Power/201609/t20160928_401072.htm" target="_blank">华润电力荣获2016年亚洲能源技术创新奖</a></li>
+		               <li><a href="http://winfo.crc.com.cn/news/crc_dynamic/201609/t20160926_400921.htm" target="_blank">华润大学举办创新论坛暨行业专家交流活动</a></li>
+		               <li><a href="http://winfo.crc.com.cn/news/crc_dynamic/201609/t20160923_400817.htm" target="_blank">罗熹总经理赴内蒙古调研并会见自治区布...</a></li>
+		                  
+		            </ul>
+		        </div>
+		        <!--end homenews-list--> 
+		      </div>
+		    </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- .col-xs-12 col-sm-12 col-md-12 -->
+<?php get_footer(); ?>
